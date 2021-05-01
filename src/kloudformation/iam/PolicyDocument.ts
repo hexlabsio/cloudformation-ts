d23aec1e2afa59ca2f232a0e952ebe179978d607
Chris Barbour <chris.barbour03@gmail.com>
2021-05-01 19:48:59 +0100

    improved handling of iam policies

--- src/kloudformation/iam/PolicyDocument.ts
+++ src/kloudformation/iam/PolicyDocument.ts
@@ -1,4 +1,6 @@
+import {Role} from "../../aws/iam/Role";
 import {Value} from "../Value";
+import {Action} from "./iamActions";
 export type Principal = '*' | { AWS: Value<string>[] } | { Federated: Value<string>[] } | { Service: Value<string>[] }
 export interface PolicyDocument {
   id?: string;
@@ -7,8 +9,8 @@ export interface PolicyDocument {
 }
 
 export interface PolicyStatement {
-  action?: Value<string> | Value<string[]>| Value<string>[];
-  notAction?: Value<string> | Value<string[]>| Value<string>[];
+  action?: Value<string> | Value<string[]>| Value<string>[] | Action | Action[];
+  notAction?: Value<string> | Value<string[]>| Value<string>[] | Action | Action[];
   effect: 'Allow' | 'Deny';
   resource?: Value<string> | Value<string[]> | Value<string>[];
   notResource?: Value<string> | Value<string[]>| Value<string>[];
@@ -19,3 +21,47 @@ export interface PolicyStatement {
 }
 
 export function iamPolicy(policyDocument: PolicyDocument): PolicyDocument { return policyDocument }
+
+export class Policy {
+  private constructor(readonly document: PolicyDocument = {statement: [], version: "2012-10-17"}) { }
+  
+  grant(action: Action, effect: PolicyStatement['effect'], onResource: PolicyStatement['resource']): this {
+    this.document.statement = [
+      ...(this.document.statement ?? []),
+      {  action, effect, resource: onResource }
+    ]
+    return this;
+  }
+  
+  allow(action: Action, onResource: PolicyStatement['resource']): this {
+    return this.grant(action, 'Allow', onResource)
+  }
+  
+  deny(action: Action, onResource: PolicyStatement['resource']): this {
+    return this.grant(action, 'Deny', onResource)
+  }
+  
+  static allow(action: Action, onResource: PolicyStatement['resource']): Policy {
+    return new Policy().allow(action, onResource);
+  }
+  
+  static deny(action: Action, onResource: PolicyStatement['resource']): Policy {
+    return new Policy().deny(action, onResource);
+  }
+}
+
+export class Iam {
+  private constructor(private role: Role) { }
+  
+  add(policyName: string, policy: Policy): this {
+    this.role.policies = [
+      ...(this.role.policies ?? []),
+      {policyName, policyDocument: policy.document}
+    ]
+    return this;
+  }
+  
+  static from(role: Role) {
+    return new Iam(role);
+  }
+}
