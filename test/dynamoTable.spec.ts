12183e471db2534df2febb372b0dde8dc59a5277
richard-gibson <rickityg@gmail.com>
2022-06-10 21:53:46 +0100

    dynamo module test

---
+++ test/dynamoTable.spec.ts
@@ -0,0 +1,91 @@
+import { Template } from "../src/kloudformation/kloudformation";
+import { dynamoTable } from "../src/kloudformation/modules/dynamo";
+import { defineTable } from "@hexlabs/dynamo-ts";
+
+describe("dynamoTable module", () => {
+  const tableDefinition = defineTable(
+    {
+      hash: "string",
+      text: "string?",
+      obj: { optional: true, object: { abc: "string", def: "number?" } },
+      arr: { optional: true, array: { object: { ghi: "number?" } } },
+      jkl: "number?",
+    },
+    "hash",
+    "text",
+    {
+      index: { hashKey: "hash", rangeKey: "jkl" },
+    }
+  );
+
+  it("should correctly render a dynamo-ts table definition", () => {
+    const output = Template.create((aws) => {
+      dynamoTable(aws, tableDefinition, "dummyTable", {
+        tags: [{ key: "tag-key", value: "tag-value" }],
+        billingMode: "PAY_PER_REQUEST",
+      });
+    }, undefined);
+    expect(output).toEqual({
+      template: {
+        Parameters: {},
+        Resources: {
+          DummyTable: {
+            Type: "AWS::DynamoDB::Table",
+            Properties: {
+              Tags: [
+                {
+                  Key: "tag-key",
+                  Value: "tag-value",
+                },
+              ],
+              BillingMode: "PAY_PER_REQUEST",
+              TableName: "dummyTable",
+              KeySchema: [
+                {
+                  KeyType: "HASH",
+                  AttributeName: "hash",
+                },
+                {
+                  KeyType: "RANGE",
+                  AttributeName: "text",
+                },
+              ],
+              AttributeDefinitions: [
+                {
+                  AttributeName: "hash",
+                  AttributeType: "S",
+                },
+                {
+                  AttributeName: "text",
+                  AttributeType: "S",
+                },
+                {
+                  AttributeName: "jkl",
+                  AttributeType: "N",
+                },
+              ],
+              GlobalSecondaryIndexes: [
+                {
+                  IndexName: "index",
+                  KeySchema: [
+                    {
+                      KeyType: "HASH",
+                      AttributeName: "hash",
+                    },
+                    {
+                      KeyType: "RANGE",
+                      AttributeName: "jkl",
+                    },
+                  ],
+                  Projection: {
+                    ProjectionType: "ALL",
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+  });
+});
