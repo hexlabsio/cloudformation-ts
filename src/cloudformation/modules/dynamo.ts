a4e5953b65094c393d11bada5c705193b8ee609d
Chris Barbour <chris.barbour@hexlabs.io>
2023-11-25 17:41:20 +0000

    allowing for capitalization in dynamo

--- src/cloudformation/modules/dynamo.ts
+++ src/cloudformation/modules/dynamo.ts
@@ -2,7 +2,7 @@ import {Table, TableProperties} from "../../aws/dynamodb/Table";
 import {Role} from "../../aws/iam/Role";
 import { AWSResourcesFor } from '../aws';
 import {Iam, Policy} from "../iam/PolicyDocument";
-import { normalize } from "../cloudformation";
+import { normalize, Value } from "../cloudformation";
 import {join} from "../Value";
 
 export function grantTableAccess(role: Role, policyName: string, tables: Table[]) {
@@ -10,12 +10,38 @@ export function grantTableAccess(role: Role, policyName: string, tables: Table[]
   Iam.from(role).add(policyName, Policy.allow("dynamodb:*", [...tables.map(table => table.attributes.Arn()), ...indexes]))
 }
 
+type LowerCase<T> =
+  T extends Value<string | boolean | number> ? T :
+  T extends (infer A)[]
+    ? LowerCase<A>[]
+    : T extends Record<string, any>
+      ? { [K in keyof T as `${Uncapitalize<K & string>}`]: LowerCase<T[K]> }
+      : T
+function camelCase<T>(obj: T): LowerCase<T> {
+  if(Array.isArray(obj)) return obj.map(camelCase) as any;
+  if(obj) {
+    if(typeof obj === 'object') {
+      return Object.keys(obj).reduce((prev, key) => ({...prev, [key.substring(0,1).toLowerCase()+key.substring(1)]: camelCase(obj[key])}), {} as any);
+    }
+  }
+  return obj as any;
+}
+
+type UpperCase<T> =
+  T extends Value<string | boolean | number> ? T :
+  T extends (infer A)[]
+    ? UpperCase<A>[]
+    : T extends Record<string, any>
+      ? { [K in keyof T as `${Capitalize<K & string>}`]: UpperCase<T[K]> }
+      : T
+
 export function dynamoTable(
   aws: AWSResourcesFor<'dynamodb'>,
-  props: TableProperties,
+  props: TableProperties | UpperCase<TableProperties>,
   name?: string
 ): Table {
-  const table = aws.dynamodb.table({...props, ...(name ? {tableName: name }: {})});
+
+  const table = aws.dynamodb.table({...camelCase(props), ...(name ? {tableName: name }: {})});
   if(name){
     return table.withLogicalName(normalize(name));
   }
